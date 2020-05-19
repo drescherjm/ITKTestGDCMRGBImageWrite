60e1aaf93da13272eaf917dfc1c1614a7731fc98
John M. Drescher <drescherjm@gmail.com>
2020-05-19 19:54:09 -0400

    Update Code.cxx
    Updated example to show the code working when the itk::GDCMImageIO object is not shared between the reader and the writer.

--- Code.cxx
+++ Code.cxx
@@ -3,16 +3,27 @@
 #include <itkImageFileReader.h>
 #include <itkImageFileWriter.h>
 #include <itkGDCMImageIO.h>
-//#include <itkExtractImageFilter.h>
+#include <type_traits>
+
+// Code crashes when the following define is enabled.
+#define SHARE_IO_OBJECT
+#ifndef SHARE_IO_OBJECT	
+#include <itkMetaDataDictionary.h>
+#include <itkMetaDataObject.h>
+#endif 
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 const unsigned int	ImageDimension2D = 2;
 
-typedef itk::RGBPixel<char> RGBPixelType;
+typedef itk::RGBPixel<unsigned char> RGBPixelType;
 typedef	itk::Image< RGBPixelType, ImageDimension2D > RGBInputImageType;
 typedef	RGBInputImageType RGBOutputImageType;
 typedef	itk::ImageFileReader< RGBInputImageType > RGBReaderType;
 typedef	itk::ImageFileWriter< RGBOutputImageType > RGBWriterType;
 typedef	itk::GDCMImageIO	ImageIOType;
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 int
 main(int argc, char* argv[])
@@ -22,6 +33,8 @@ main(int argc, char* argv[])
 		std::cerr << "Usage: " << argv[0] << " DicomFile " << std::endl;
 		return EXIT_FAILURE;
 	}
+
+	std::cout << std::boolalpha << "A char is signed: " << std::is_signed<char>::value << std::endl;
 
 	RGBReaderType::Pointer reader = RGBReaderType::New();
 
@@ -34,14 +47,34 @@ main(int argc, char* argv[])
 
 	gdcmImageIO->KeepOriginalUIDOn();
 
+	try
+	{
+		reader->Update();
+	}
+	catch (itk::ExceptionObject& e)
+	{
+		std::cerr << "exception in file reader " << std::endl;
+		std::cerr << e << std::endl;
+		return false;
+	}
+
 	fileName += ".new.dcm";
 
 	RGBWriterType::Pointer writer = RGBWriterType::New();
 	writer->SetFileName(fileName);
 	writer->SetInput(reader->GetOutput());
-	writer->UseInputMetaDataDictionaryOff();
-	//writer->UseInputMetaDataDictionaryOn();
+
+#ifdef SHARE_IO_OBJECT	
 	writer->SetImageIO(gdcmImageIO);
+#else  // def SHARE_IO_OBJECT
+	
+	// If not sharing itk::GDCMImageIO we copy the dictionary separately
+	auto dict = gdcmImageIO->GetMetaDataDictionary();
+	ImageIOType::Pointer gdcmImageIO1 = ImageIOType::New();
+	writer->SetImageIO(gdcmImageIO1);
+
+	gdcmImageIO1->SetMetaDataDictionary(dict);
+#endif // def SHARE_IO_OBJECT
 
 	try
 	{
@@ -53,6 +86,4 @@ main(int argc, char* argv[])
 		std::cerr << e << std::endl;
 		return false;
 	}
-
-
 }
